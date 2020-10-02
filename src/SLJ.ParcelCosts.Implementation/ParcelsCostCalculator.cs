cf9a3cc84e29845451aad70a3730092f4394292b
Struan Judd <github@scifi.geek.nz>
2020-10-03 10:30:22 +1300

    Refactor to improve code
    Also remove unneccesary edge cases

--- src/SLJ.ParcelCosts.Implementation/ParcelsCostCalculator.cs
+++ src/SLJ.ParcelCosts.Implementation/ParcelsCostCalculator.cs
@@ -1,10 +1,21 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SLJ.ParcelCosts.Implementation
 {
   public class ParcelsCostCalculator : IParcelsCostCalculator
   {
+    const decimal SmallMaxDimension = 10;
+    const decimal MediumMaxDimension = 50;
+    const decimal LargeMaxDimension = 100;
+
+    readonly IReadOnlyDictionary<ParcelCostingType, decimal> ParcelCosts = new Dictionary<ParcelCostingType, decimal> {
+      [ParcelCostingType.Small] = 3,
+      [ParcelCostingType.Medium] = 8,
+      [ParcelCostingType.Large] = 15,
+      [ParcelCostingType.ExtraLarge] = 25,
+    };
+
     public IOrderCosting CalculateCosts(IOrder order)
     {
       var parcelCosts = order.Parcels.Select(CalculateParcelCost).ToList();
@@ -14,37 +25,25 @@ namespace SLJ.ParcelCosts.Implementation
       };
     }
 
-    private static ParcelCosting CalculateParcelCost(IParcel parcel)
+    ParcelCosting CalculateParcelCost(IParcel parcel)
     {
       var parcelCosting = new ParcelCosting {
         Parcel = parcel,
         CostingType = CalculateParcelCostingType(parcel)
       };
 
-      switch ( parcelCosting.CostingType ) {
-        case ParcelCostingType.Small:
-          parcelCosting.ParcelCost = 3;
-          break;
-        case ParcelCostingType.Medium:
-          parcelCosting.ParcelCost = 8;
-          break;
-        case ParcelCostingType.Large:
-          parcelCosting.ParcelCost = 15;
-          break;
-        case ParcelCostingType.Unknown:
-        case ParcelCostingType.ExtraLarge:
-        default:
-          parcelCosting.ParcelCost = 25;
-          break;
-      }
+      parcelCosting.ParcelCost = ParcelCosts[parcelCosting.CostingType];
 
       return parcelCosting;
     }
 
-    private static ParcelCostingType CalculateParcelCostingType(IParcel parcel)
-      => parcel.Height < 10 && parcel.Width < 10 && parcel.Depth < 10 ? ParcelCostingType.Small
-        : parcel.Height < 50 && parcel.Width < 50 && parcel.Depth < 50 ? ParcelCostingType.Medium
-        : parcel.Height < 100 && parcel.Width < 100 && parcel.Depth < 100 ? ParcelCostingType.Large
+    ParcelCostingType CalculateParcelCostingType(IParcel parcel)
+      => AllParcelDimensionsLessThan(parcel, SmallMaxDimension) ? ParcelCostingType.Small
+        : AllParcelDimensionsLessThan(parcel, MediumMaxDimension) ? ParcelCostingType.Medium
+        : AllParcelDimensionsLessThan(parcel, LargeMaxDimension) ? ParcelCostingType.Large
         : ParcelCostingType.ExtraLarge;
+
+    bool AllParcelDimensionsLessThan(IParcel parcel, decimal maxDimension)
+      => parcel.Height < maxDimension && parcel.Width < maxDimension && parcel.Depth < maxDimension;
   }
 }
