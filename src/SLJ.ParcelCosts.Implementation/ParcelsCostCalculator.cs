784709ce276a7de324373a26cecdd755efe44a4e
Struan Judd <github@scifi.geek.nz>
2020-10-03 10:14:03 +1300

    Other sizes tests pass

--- src/SLJ.ParcelCosts.Implementation/ParcelsCostCalculator.cs
+++ src/SLJ.ParcelCosts.Implementation/ParcelsCostCalculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace SLJ.ParcelCosts.Implementation
@@ -5,11 +6,45 @@ namespace SLJ.ParcelCosts.Implementation
   public class ParcelsCostCalculator : IParcelsCostCalculator
   {
     public IOrderCosting CalculateCosts(IOrder order)
-      => new OrderCosting {
-        TotalCost = 3,
-        ParcelCosts = order.Parcels.Select(p => new ParcelCosting {
-          Parcel = p, ParcelCost = 3, CostingType = ParcelCostingType.Small
-        })
+    {
+      var parcelCosts = order.Parcels.Select(CalculateParcelCost).ToList();
+      return new OrderCosting {
+        TotalCost = parcelCosts.Sum(p => p.ParcelCost),
+        ParcelCosts = parcelCosts
       };
+    }
+
+    private static ParcelCosting CalculateParcelCost(IParcel parcel)
+    {
+      var parcelCosting = new ParcelCosting {
+        Parcel = parcel,
+        CostingType = CalculateParcelCostingType(parcel)
+      };
+
+      switch ( parcelCosting.CostingType ) {
+        case ParcelCostingType.Small:
+          parcelCosting.ParcelCost = 3;
+          break;
+        case ParcelCostingType.Medium:
+          parcelCosting.ParcelCost = 8;
+          break;
+        case ParcelCostingType.Large:
+          parcelCosting.ParcelCost = 15;
+          break;
+        case ParcelCostingType.Unknown:
+        case ParcelCostingType.ExtraLarge:
+        default:
+          parcelCosting.ParcelCost = 25;
+          break;
+      }
+
+      return parcelCosting;
+    }
+
+    private static ParcelCostingType CalculateParcelCostingType(IParcel parcel)
+      => parcel.Height < 10 && parcel.Width < 10 && parcel.Depth < 10 ? ParcelCostingType.Small
+        : parcel.Height < 50 && parcel.Width < 50 && parcel.Depth < 50 ? ParcelCostingType.Medium
+        : parcel.Height < 100 && parcel.Width < 100 && parcel.Depth < 100 ? ParcelCostingType.Large
+        : ParcelCostingType.ExtraLarge;
   }
 }
